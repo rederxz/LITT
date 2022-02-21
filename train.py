f6897392497f375e5ffe5c5920d0ac549894525e
Rederxz <1148002188@qq.com>
2022-02-21 15:22:16 +0800

    Bug fix.

--- train.py
+++ train.py
@@ -47,17 +47,96 @@ def prep_input(im, **kwargs):
     return im_und_l, k_und_l, mask_l, im_gnd_l
 
 
+def step_train(data_generator, model, criterion, optimizer):
+    train_loss = 0
+    train_batches = 0
+    model.train()
+    for im in data_generator:
+        im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
+        if torch.cuda.is_available():
+            im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
+
+        rec = model(im_u, k_u, mask)
+        loss = criterion(rec, gnd)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        train_batches += 1
+
+    train_loss /= train_batches
+
+    print(time.strftime('%H:%M:%S') + ' ' + f'train'
+          + ' - ' + f'loss: {train_loss}')
+
+
+def step_validate(data_generator, model, criterion):
+    validate_loss = 0
+    validate_batches = 0
+    model.eval()
+    for im in data_generator:
+        with torch.no_grad():
+            im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
+            if torch.cuda.is_available():
+                im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
+            pred = model(im_u, k_u, mask)
+            loss = criterion(pred, gnd)
+
+        validate_loss += loss.item()
+        validate_batches += 1
+
+    validate_loss /= validate_batches
+
+    print(time.strftime('%H:%M:%S') + ' ' + f'valid'
+          + ' - ' + f'loss: {validate_loss}')
+
+
+def step_test(data_generator, model, criterion):
+    test_loss = 0
+    base_psnr = 0
+    test_psnr = 0
+    test_batches = 0
+    for im in data_generator:
+        with torch.no_grad():
+            im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
+            if torch.cuda.is_available():
+                im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
+            pred = model(im_u, k_u, mask)
+            loss = criterion(pred, gnd)
+
+        test_loss += loss.item()
+        test_batches += 1
+
+        for im_i, und_i, pred_i in zip(im,
+                                       from_tensor_format(im_u.cpu().numpy()),
+                                       from_tensor_format(pred.cpu().numpy())):
+            base_psnr += complex_psnr(im_i, und_i, peak='max')
+            test_psnr += complex_psnr(im_i, pred_i, peak='max')
+
+    test_loss /= test_batches
+    base_psnr /= test_batches * 1  # "1" for iteration within each mini-batch
+    test_psnr /= test_batches * 1
+    print(time.strftime('%H:%M:%S') + ' ' + f'test'
+          + ' - ' + f'loss: {test_loss}'
+          + ', ' + f'base PSNR: {base_psnr}'
+          + ', ' + f'test PSNR: {test_psnr}')
+
+    # save model
+    torch.save(rec_net.state_dict(), os.path.join(args.work_dir, f'model.pth'))
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', type=str, default='./LITT_data/', help='the directory of data')
     parser.add_argument('--num_epoch', type=int, default=200, help='number of epochs')
     parser.add_argument('--batch_size', type=int, default=1, help='batch size')
-    # parser.add_argument('--num_batch', type=int, default=10, help='number of batches in each epoch')  # TODO why?
     parser.add_argument('--lr', type=float, default=0.0001, help='initial learning rate')
     parser.add_argument('--acc', type=float, default=6.0, help='Acceleration factor for k-space sampling')
     parser.add_argument('--sampled_lines', type=int, default=15, help='Number of sampled lines at k-space center')
     parser.add_argument('--nt_network', type=int, default=5, help='Time frames involved in the network.')
-    parser.add_argument('--save_every', type=int, default=20, help='Epoch intervals to save')
+    parser.add_argument('--test_interval', type=int, default=20, help='Epoch intervals to test')
     parser.add_argument('--work_dir', type=str, default='crnn', help='work directory')
     parser.add_argument('--debug', action='store_true', help='debug mode')
     # parser.add_argument('--save_fig', action='store_true', help='Save output images and masks')  # TODO
@@ -68,17 +147,15 @@ if __name__ == '__main__':
     # rotation_xy = False
     # flip_t = False
 
-    # create work directory if needed
+    # create work directory
     if not os.path.exists(args.work_dir):
         os.mkdir(args.work_dir)
 
-    # dataset, sample shape [n_samples, x, y, t]
+    # data
     train = load_data_v2(data_path=args.data_path,
-                         split='train', nt_network=args.nt_network)
-    validate = load_data_v2(data_path=args.data_path,
-                         split='validation', nt_network=args.nt_network)
-    test = load_data_v2(data_path=args.data_path,
-                         split='test', nt_network=args.nt_network)
+                         split='train', nt_network=args.nt_network)  # each sample [n_samples, x, y, t]
+    validate = load_data_v2(data_path=args.data_path, split='validation', nt_network=args.nt_network)  # ...
+    test = load_data_v2(data_path=args.data_path, split='test', nt_network=args.nt_network)  # ...
 
     # TODO: 为什么不是在线的数据增强？
     # if aug:
@@ -86,107 +163,29 @@ if __name__ == '__main__':
     #     validate = data_aug(validate, block_size, rotation_xy, flip_t)
     #     test = data_aug(test, block_size, rotation_xy, flip_t)  # TODO: test 为什么还需要数据增强?
 
-    # sample shape => [n_samples, t, x, y]
-    train = train.transpose((0, 3, 1, 2))
-    validate = validate.transpose((0, 3, 1, 2))
-    test = test.transpose((0, 3, 1, 2))
+    train = train.transpose((0, 3, 1, 2))  # each sample => [n_samples, t, x, y]
+    validate = validate.transpose((0, 3, 1, 2))  # ...
+    test = test.transpose((0, 3, 1, 2))  # ...
+
+    # TODO: 为什么不使用 dataloader？
+    train_generator = iterate_minibatch(data=train, batch_size=args.batch_size, shuffle=True)
+    validate_generator = iterate_minibatch(data=validate, batch_size=1, shuffle=False)
+    test_generator = iterate_minibatch(data=test, batch_size=1, shuffle=False)
 
     # model, loss, optimizer
     rec_net = CRNN_MRI_UniDir()
     criterion = torch.nn.MSELoss()
     optimizer = torch.optim.Adam(rec_net.parameters(), lr=args.lr, betas=(0.5, 0.999))
 
+    # device
     if torch.cuda.is_available():
         rec_net = rec_net.cuda()
         criterion.cuda()
 
-    # training & validation
+    # training, validation & test
     for epoch in range(args.num_epoch):
         print(f'Epoch {epoch + 1}/{args.num_epoch}')
-
-        # train step
-        train_loss = 0
-        train_batches = 0
-        rec_net.train()
-        for im in iterate_minibatch(data=train, batch_size=args.batch_size, shuffle=True):  # TODO: 为什么不使用dataloader？
-            im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
-            if torch.cuda.is_available():
-                im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
-
-            rec = rec_net(im_u, k_u, mask)
-            loss = criterion(rec, gnd)
-
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            train_loss += loss.item()
-            train_batches += 1
-
-            if args.debug and train_batches == 20:
-                break
-        train_loss /= train_batches
-        print(time.strftime('%H:%M:%S') + ' ' + f'train'
-              + ' - ' + f'loss: {train_loss}')
-
-        # validation step
-        validate_loss = 0
-        validate_batches = 0
-        rec_net.eval()
-        for im in iterate_minibatch(data=validate, batch_size=1, shuffle=False):
-            with torch.no_grad():
-                im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
-                if torch.cuda.is_available():
-                    im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
-                pred = rec_net(im_u, k_u, mask)
-                loss = criterion(pred, gnd)
-
-            validate_loss += loss.item()
-            validate_batches += 1
-
-            if args.debug and validate_batches == 20:
-                break
-        validate_loss /= validate_batches
-        print(time.strftime('%H:%M:%S') + ' ' + f'valid'
-              + ' - ' + f'loss: {validate_loss}')
-
-        # test step
-        if (epoch + 1) % args.save_every != 0:
-            continue
-
-        # save model
-        torch.save(rec_net.state_dict(), os.path.join(args.work_dir, f'model_epoch_{epoch + 1}.pth'))
-
-        # save test results
-        vis = []
-        test_loss = 0
-        base_psnr = 0
-        test_psnr = 0
-        test_batches = 0
-        for im in iterate_minibatch(data=test, batch_size=1, shuffle=False):  # TODO 原本batch_size=3是什么操作？
-            with torch.no_grad():
-                im_u, k_u, mask, gnd = prep_input(im, acc=args.acc, sample_n=args.sampled_lines)
-                if torch.cuda.is_available():
-                    im_u, k_u, mask, gnd = im_u.cuda(), k_u.cuda(), mask.cuda(), gnd.cuda()
-                pred = rec_net(im_u, k_u, mask)
-                loss = criterion(pred, gnd)
-
-            test_loss += loss.item()
-            test_batches += 1
-
-            for im_i, und_i, pred_i in zip(im,
-                                           from_tensor_format(im_u.cpu().numpy()),
-                                           from_tensor_format(pred.cpu().numpy())):
-                base_psnr += complex_psnr(im_i, und_i, peak='max')
-                test_psnr += complex_psnr(im_i, pred_i, peak='max')
-
-            if args.debug and test_batches == 20:
-                break
-
-        test_loss /= test_batches
-        base_psnr /= test_batches * 1  # "1" for iteration within each mini-batch
-        test_psnr /= test_batches * 1
-        print(time.strftime('%H:%M:%S') + ' ' + f'test'
-              + ' - ' + f'loss: {test_loss}'
-              + ', ' + f'base PSNR: {base_psnr}'
-              + ', ' + f'test PSNR: {test_psnr}')
+        step_train(train_generator, rec_net, criterion, optimizer)
+        step_validate(validate_generator, rec_net, criterion)
+        if (epoch + 1) % args.test_interval == 0:
+            step_test(test_generator, rec_net, criterion)
