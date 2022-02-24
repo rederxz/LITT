8abfb1039fbf7e382475a0e5ef017b66d13122bb
Rederxz <1148002188@qq.com>
2022-02-25 00:15:22 +0800

    Add use specified mask in data.py.

--- data.py
+++ data.py
@@ -11,65 +11,79 @@ import compressed_sensing as cs
 def cut_data(input, block_size=(1, 1), cut_edge=None):
     """
     make sure input is divisible by the block size and cut edge area
-    :param input: with shape [n_samples, echo, x, y, nt]
+    :param input: with shape [n_samples, (echo,)nt, x, y]
     :param block_size: [int, int], such as [256, 32]
     :param cut_edge: [0, 32]
     :return:
     """
     if cut_edge is not None:
         if cut_edge[0] != 0:
-            input = input[..., cut_edge[0]:-cut_edge[0], :, :]
+            input = input[..., cut_edge[0]:-cut_edge[0], :]
         if cut_edge[1] != 0:
-            input = input[..., :, cut_edge[1]:-cut_edge[1], :]
+            input = input[..., :, cut_edge[1]:-cut_edge[1]]
 
     # make sure x dim divisible
     start_x, end_x = 0, None
-    assert input.shape[-3] >= block_size[0]
-    remainder = input.shape[-3] % block_size[0]
+    assert input.shape[-2] >= block_size[0]
+    remainder = input.shape[-2] % block_size[0]
     if remainder > 0:
         start_x += remainder // 2
         end_x = - (remainder - remainder // 2)
 
     # make sure y dim divisible
     start_y, end_y = 0, None
-    assert input.shape[-2] >= block_size[1]
-    remainder = input.shape[-2] % block_size[1]
+    assert input.shape[-1] >= block_size[1]
+    remainder = input.shape[-1] % block_size[1]
     if remainder > 0:
         start_y += remainder // 2
         end_y = - (remainder - remainder // 2)
 
-    input_p = input[..., start_x:end_x, start_y:end_y, :]
+    input_p = input[..., start_x:end_x, start_y:end_y]
 
     return input_p
 
 
 @ curry
-def data_aug(img, block_size=None, rotation_xy=False, flip_t=False):
+def data_aug(img, mask=None, block_size=None, rotation_xy=False, flip_t=False):
+    # FIXME how to apply change to img and mask simultaneously?
     """
     apply data augmentation to a batch
-    :param img: [n, x, y, nt] or [n, echo, x, y, nt]
+    :param img: [n, (echo, )nt, x, y]
+    :param mask: must be the same shape with img
     :param block_size: [int, int]
     :param rotation_xy: boolean
     :param flip_t: boolean
     :return:
     """
+    if mask is not None:
+        assert mask.shape == img.shape
+
     if rotation_xy and np.rand.random() > 0.5:
-        img = np.rot90(img, k=np.random.randint(0, 4), axes=(-3, -2))
+        k = np.random.randint(0, 4)
+        img = np.rot90(img, k=k, axes=(-2, -1))
+        if mask is not None:
+            mask = np.rot90(mask, k=k, axes=(-2, -1))
 
     if flip_t and np.rand.random() > 0.5:
-        img = img[..., ::-1]
+        img = img[..., ::-1, :, :]
+        if mask is not None:
+            mask = mask[..., ::-1, :, :]
 
     if block_size is not None:
-        assert img.shape[-3] >= block_size[0] and img.shape[-2] >= block_size[1]
+        assert img.shape[-2] >= block_size[0] and img.shape[-1] >= block_size[1]
 
         # cut unimportant edge area
         img = cut_data(img, cut_edge=(0, 32))
+        if mask is not None:
+            mask = cut_data(mask, cut_edge=(0, 32))
 
         start_x = np.random.randint(0, img.shape[-3] - block_size[0] + 1)
         start_y = np.random.randint(0, img.shape[-2] - block_size[1] + 1)
-        img = img[..., start_x: start_x + block_size[0], start_y: start_y + block_size[1], :]
+        img = img[..., start_x: start_x + block_size[0], start_y: start_y + block_size[1]]
+        if mask is not None:
+            mask = mask[..., start_x: start_x + block_size[0], start_y: start_y + block_size[1]]
 
-    return img
+    return img if mask is None else (img, mask)
 
 
 def load_data(type, nt_network, load_one_data_ind=None):
@@ -191,7 +205,7 @@ def load_data_v2(data_path, split, nt_network=None, single_echo=True):
 
 class LITT(torch.utils.data.dataset.Dataset):
     def __init__(self, mat_file_path, nt_network=None, single_echo=True, acc=6.0, sample_n=8,
-                 mask=None, overlap=False, transform=None):
+                 mask_file_path=None, overlap=False, transform=None):
         """
         build a LITT dataset
         :param mat_file_path: a list, the paths of mat files
@@ -199,9 +213,9 @@ class LITT(torch.utils.data.dataset.Dataset):
         :param single_echo: if only use one echo
         :param acc: accelerating rate
         :param sample_n: preserve how many center lines (sample_n // 2 each side)
-        :param mask: a given mask with shape [..., x, y]
+        :param mask: a given mask with shape [..., t, x, y]
         :param overlap: if samples overlap with each other along time dimension
-        :param transform: transform applied to each sample, need to keep shape [..., x, y, time]
+        :param transform: transform applied to each sample, need to keep shape [...,time, x, y]
         :return: each sample has shape [(echo,) 2, x, y, time], 'time' size is set to nt_network if specified
         """
         super(LITT, self).__init__()
@@ -209,45 +223,51 @@ class LITT(torch.utils.data.dataset.Dataset):
         self.acc = acc
         self.sample_n = sample_n
         self.transform = transform
-        self.mask = mask
+        if mask_file_path is not None:
+            assert len(mat_file_path) == len(mask_file_path)
 
         self.data = list()
-        for file_path in mat_file_path:
+        self.mask = list()
+        for idx, file_path in enumerate(mat_file_path):
             mat_data = loadmat(file_path)
             mFFE_img_imag = mat_data['mFFE_img_imag']
             mFFE_img_real = mat_data['mFFE_img_real']
             mFFE_img_complex = mFFE_img_real + 1j * mFFE_img_imag  # [x, y, time, echo]
             mFFE_img_complex = np.transpose(
-                mFFE_img_complex, (3, 0, 1, 2))  # -> [echo, x, y, time] to adapt to following processing in __getitem__
+                mFFE_img_complex, (3, 2, 0, 1))  # -> [echo, time, x, y]
 
             if single_echo:
                 mFFE_img_complex = mFFE_img_complex[1]  # if single echo, use the second one
 
+            if mask_file_path is not None:
+                mask = loadmat(mask_file_path[idx])['mask'].reshape(mFFE_img_complex.shape)
+            else:
+                mask = cs.cartesian_mask(mFFE_img_complex.shape, acc=self.acc, sample_n=self.sample_n)
+
             if nt_network is None:
                 self.data.append(mFFE_img_complex)
+                self.mask.append(mask)
             elif not overlap:  # slice the data along time dim according to nt_network with no overlapping
-                total_t = mFFE_img_complex.shape[-1]
+                total_t = mFFE_img_complex.shape[-3]
                 complete_slice = total_t // nt_network
                 for i in range(complete_slice):
-                    self.data.append(mFFE_img_complex[..., i * nt_network:(i + 1) * nt_network])
+                    self.data.append(mFFE_img_complex[..., i * nt_network:(i + 1) * nt_network, :, :])
+                    self.mask.append(mask[..., i * nt_network:(i + 1) * nt_network, :, :])
                 if total_t % nt_network > 0:
-                    self.data.append(mFFE_img_complex[..., -nt_network:])
+                    self.data.append(mFFE_img_complex[..., -nt_network:, :, :])
+                    self.mask.append(mask[..., -nt_network:, :, :])
             else:  # ... with overlapping
                 for i in range(mFFE_img_complex.shape[-1] - (nt_network - 1)):
-                    self.data.append(mFFE_img_complex[..., i:i + nt_network])
+                    self.data.append(mFFE_img_complex[..., i:i + nt_network, :, :])
+                    self.mask.append(mask[..., i:i + nt_network, :, :])
 
     def __getitem__(self, idx):
-        img_gnd = self.data[idx]  # [(echo, )x, y, time]
+        img_gnd = self.data[idx]  # [(echo, )time, x, y]
+        mask = self.mask[idx]
 
         if self.transform is not None:
-            img_gnd = self.transform(img_gnd)
+            img_gnd, mask = self.transform(img_gnd, mask)
 
-        # note: mask generation and under-sampling require img with shape [..., x, y]
-        img_gnd = np.transpose(img_gnd, (2, 0, 1)) if self.single_echo else np.transpose(img_gnd, (0, 3, 1, 2))
-        if self.mask is not None:
-            mask = self.mask
-        else:
-            mask = cs.cartesian_mask(img_gnd.shape, acc=self.acc, sample_n=self.sample_n)
         img_u, k_u = cs.undersample(img_gnd, mask)
 
         # complex64 -> float32, [(echo, )time, x, y] -> [(echo, )time, x, y, 2] -> [(echo,) 2, x, y, time]
