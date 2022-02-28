d229a5bc58456b3dc8807b26fd9e76a6d796d5b3
Rederxz <1148002188@qq.com>
2022-02-28 09:28:54 +0800

    Modify inference.py for LITT dataset change.

--- inference.py
+++ inference.py
@@ -3,7 +3,6 @@ import os
 import subprocess
 import sys
 import time
-from collections import deque
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,13 +28,8 @@ def plot_diff(img_gnd, img_u, img_rec):
     return amp_diff, phase_diff
 
 
-def step_inference(dataloader, model, work_dir, fig_interval, queue_mode, nt_network, nt_wait=0):
+def step_inference(dataloader, model, work_dir, fig_interval, queue_mode, nt_wait):
     img_gnd_all, img_u_all, mask_all, img_rec_all, t_rec_all = list(), list(), list(), list(), list()
-
-    img_u_pool = deque([], maxlen=nt_network)
-    k_u_pool = deque([], maxlen=nt_network)
-    mask_pool = deque([], maxlen=nt_network)
-    img_gnd_pool = deque([], maxlen=nt_network)
 
     model.eval()
     for i, batch in enumerate(dataloader):
@@ -44,24 +38,7 @@ def step_inference(dataloader, model, work_dir, fig_interval, queue_mode, nt_net
             if torch.cuda.is_available():
                 img_u, k_u, mask, img_gnd = img_u.cuda(), k_u.cuda(), mask.cuda(), img_gnd.cuda()
 
-            img_u_pool.append(img_u)
-            k_u_pool.append(k_u)
-            mask_pool.append(mask)
-            img_gnd_pool.append(img_gnd)
-
-            if i == nt_wait - 1:
-                wanted_start = 0
-                time_record = nt_wait
-            elif i > nt_wait - 1:
-                wanted_start = -1
-                time_record = 1
-            else:
-                continue
-
-            img_u = torch.cat(list(img_u_pool), dim=-1)
-            k_u = torch.cat(list(k_u_pool), dim=-1)
-            mask = torch.cat(list(mask_pool), dim=-1)
-            img_gnd = torch.cat(list(img_gnd_pool), dim=-1)
+            wanted_start, time_record = (-1, 1) if img_gnd.shape[-3] != nt_wait else (0, nt_wait)
 
             tik = time.time()
             if queue_mode:
@@ -93,19 +70,22 @@ def step_inference(dataloader, model, work_dir, fig_interval, queue_mode, nt_net
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--work_dir', type=str, default='crnn', help='work directory')
+    # -- data --
     parser.add_argument('--data_path', type=str, default='./LITT_data/', help='the directory of data')
-    parser.add_argument('--mask_path', type=str, nargs='+', help='the path of the specified mask')
-    parser.add_argument('--model_path', type=str, default='./crnn/model.pth', help='the path of model weights')
-    parser.add_argument('--queue_mode', action='store_true',
-                        help='if inference one frame by one frame when testing unidirectional model')
-    parser.add_argument('--batch_size', type=int, default=1, help='batch size')
     parser.add_argument('--acc', type=float, default=6.0, help='Acceleration factor for k-space sampling')
     parser.add_argument('--sampled_lines', type=int, default=8, help='Number of sampled lines at k-space center')
+    parser.add_argument('--mask_path', type=str, nargs='+', help='the path of the specified mask')
+    # -- model --
+    parser.add_argument('--model_path', type=str, default='./crnn/model.pth', help='the path of model weights')
     parser.add_argument('--uni_direction', action='store_true', help='Bidirectional or unidirectional network')
     parser.add_argument('--multi_hidden_t', type=int, default=1, help='Number of hidden_t involved in the model')
-    parser.add_argument('--nt_network', type=int, default=6, help='Time frames involved in the network.')
+    # -- inference setting --
+    parser.add_argument('--nt_network', type=int, default=1, help='Time frames involved in the network.')
+    parser.add_argument('--queue_mode', action='store_true',
+                        help='if inference one frame by one frame when testing unidirectional model')
     parser.add_argument('--nt_wait', type=int, default=0)
+    # -- others --
+    parser.add_argument('--work_dir', type=str, default='crnn', help='work directory')
     parser.add_argument('--fig_interval', type=int, default=10, help='Frame intervals to save figs.')
     parser.add_argument('--debug', action='store_true', help='debug mode')
     args = parser.parse_args()
@@ -128,9 +108,9 @@ if __name__ == '__main__':
     print(vars(args))
 
     # data, each sample [n_samples(, echo), t, x, y]
-    test_dataset = get_LITT_dataset(data_root=args.data_path, split='test', nt_network=1,
+    test_dataset = get_LITT_dataset(data_root=args.data_path, split='test', nt_network=args.nt_network,
                                     single_echo=True, acc=args.acc, sample_n=args.sampled_lines,
-                                    mask_file_path=args.mask_file_path, overlap=True)
+                                    mask_file_path=args.mask_path, overlap=True, nt_wait=args.nt_wait)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=2)
 
     # model
@@ -143,4 +123,4 @@ if __name__ == '__main__':
 
     # test
     step_inference(test_loader, rec_net, args.work_dir, args.fig_interval, queue_mode=args.queue_mode,
-                   nt_network=args.nt_network, nt_wait=args.nt_wait)
+                   nt_wait=args.nt_wait)
