e16c8b4c774a8e97ef317fcfdcc54c87891ac013
Rederxz <1148002188@qq.com>
2022-03-24 17:39:51 +0800

    Add CRNN_T in model.py.

--- model.py
+++ model.py
@@ -336,3 +336,134 @@ class resCRNN(CRNN):
         self.conv1_h = nn.Identity()
         self.conv2_h = nn.Identity()
         self.conv3_h = nn.Identity()
+
+
+class CRNNcell_t(nn.Module):
+    def __init__(self, input_size, hidden_size, kernel_size, multi_hidden_t=1):
+        """
+        Only connect in time dim
+        """
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.multi_hidden_t = multi_hidden_t
+        # image2hidden conv
+        self.i2h = nn.Conv2d(input_size, hidden_size, kernel_size, padding=self.kernel_size // 2)
+        # hidden(from the neighbour frame)2hidden conv
+        self.h2h = nn.ModuleList()
+        for i in range(multi_hidden_t):
+            self.h2h.append(nn.Conv2d(hidden_size, hidden_size, kernel_size, padding=self.kernel_size // 2))
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, input, hidden_i, hidden_t):
+        """
+        :param input: the input from the previous layer, with shape [batch_size, input_size, x, y]
+        :param hidden_i: the hidden states of the previous iteration, with shape [batch_size, hidden_size, x, y]
+        :param hidden_t: torch tensor or an iterable container of torch tensor(s), the hidden states of the neighbour
+        frame(s), with shape [batch_size, hidden_size, x, y]
+        :return: hidden state with shape [batch_size, hidden_size, width, height]
+        """
+        in_to_hid = self.i2h(input)
+
+        # make sure the given hidden_t matches self.multi_hidden_t
+        if not isinstance(hidden_t, deque):
+            hidden_t = deque([hidden_t])
+        assert self.multi_hidden_t == len(hidden_t)
+
+        hid_to_hid = torch.zeros_like(hidden_i)
+        for i in range(self.multi_hidden_t):
+            hid_to_hid += self.h2h[i](torch.zeros_like(hidden_i) if hidden_t[i] is None else hidden_t[i])
+
+        hidden_out = self.relu(in_to_hid + hid_to_hid)
+
+        return hidden_out
+
+
+class CRNN_t(CRNN_t_i):
+    def __init__(self, input_size, hidden_size, kernel_size, uni_direction=False, multi_hidden_t=1):
+        super().__init__(input_size, hidden_size, kernel_size, uni_direction, multi_hidden_t)
+        self.CRNN_model = CRNNcell_t(self.input_size, self.hidden_size, self.kernel_size, multi_hidden_t)
+
+
+class CRNN_T(CRNN):
+    def __init__(self, n_ch=2, nf=64, ks=3, nc=5, nd=5, uni_direction=False, multi_hidden_t=1):
+        """
+        Model for Dynamic MRI Reconstruction using Convolutional Neural Networks
+        unidirectional version
+        :param n_ch: number of channels
+        :param nf: number of filters
+        :param ks: kernel size
+        :param nc: number of iterations
+        :param nd: number of CRNN/BCRNN/CNN layers in each iteration
+        :param uni_direction: ...
+        :param multi_hidden_t: ...
+        """
+        super().__init__()
+        self.n_ch = n_ch
+        self.nc = nc
+        self.nd = nd
+        self.nf = nf
+        self.ks = ks
+        self.uni_direction = uni_direction
+        self.multi_hidden_t = multi_hidden_t
+
+        self.crnn_t = CRNN_t(n_ch, nf, ks, uni_direction, multi_hidden_t)
+        self.conv1_x = nn.Conv2d(nf, nf, ks, padding=ks // 2)
+        self.conv2_x = nn.Conv2d(nf, nf, ks, padding=ks // 2)
+        self.conv3_x = nn.Conv2d(nf, nf, ks, padding=ks // 2)
+        self.conv4_x = nn.Conv2d(nf, n_ch, ks, padding=ks // 2)
+        self.relu = nn.ReLU(inplace=True)
+
+        dcs = []
+        for i in range(nc):
+            dcs.append(DataConsistencyInKspace(norm='ortho'))
+        self.dcs = dcs
+
+    def forward(self, x, k, m):
+        """
+        :param x: input in image domain, [batch_size, 2, width, height, n_seq]
+        :param k: initially sampled elements in k-space, [batch_size, 2, width, height, n_seq]
+        :param m: corresponding nonzero location, [batch_size, 2, width, height, n_seq]
+        :return: reconstruction result, [batch_size, 2, width, height, n_seq]
+        """
+        net = {}
+        n_batch, n_ch, width, height, n_seq = x.size()
+        size_h = [n_seq * n_batch, self.nf, width, height]
+        hid_init = x.new_zeros(size_h)  # the initial zero-valued hidden state (the same device and dtype as x)
+
+        # initialize hidden states
+        for j in range(self.nd - 1):  # except for the last vanilla CNN layer, all layers maintain a hidden state
+            net['t0_x%d' % j] = hid_init  # 't' means iteration here
+
+        # iterate
+        for i in range(1, self.nc + 1):  # i: number of iteration
+            x = x.permute(4, 0, 1, 2, 3)  # [n_seq, batch, n_ch, width, height]
+
+            net['t%d_x0' % (i - 1)] = net['t%d_x0' % (i - 1)] \
+                .view(n_seq, n_batch, self.nf, width, height)  # [n_seq, n_batch, self.nf, width, height] as required by CRNN_t_i
+
+            # 1 layer of CRNN-t
+            net['t%d_x0' % i] = self.crnn_t(x, net['t%d_x0' % (i - 1)])
+
+            net['t%d_x0' % i] = net['t%d_x0' % i] \
+                .view(-1, self.nf, width, height)  # [n_seq * n_batch, self.nf, width, height] as required by following CNN layers
+
+            # 3 layers of CRNN-i
+            net['t%d_x1' % i] = self.relu(self.conv1_x(net['t%d_x0' % i]))
+            net['t%d_x2' % i] = self.relu(self.conv2_x(net['t%d_x1' % i]))
+            net['t%d_x3' % i] = self.relu(self.conv3_x(net['t%d_x2' % i]))
+
+            # 1 layer of vanilla CNN
+            net['t%d_x4' % i] = self.conv4_x(net['t%d_x3' % i])
+
+            # shortcut connection
+            net['t%d_out' % i] = x + net['t%d_x4' % i].view(n_seq, n_batch, n_ch, width, height)
+
+            net['t%d_out' % i] = net['t%d_out' % i].permute(1, 2, 3, 4, 0)  # (batch_size, n_ch, width, height, n_seq)
+
+            x = self.dcs[i - 1].perform(net['t%d_out' % i], k, m)  # data consistency layer
+
+        return x
+
+    def queue_forward(self, x, k, m, h=None):
+        pass
+
